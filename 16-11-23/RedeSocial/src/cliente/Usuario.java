c79cda3adbd3c97c8586a209b6ce8ff00a43b6dc
Leandro Garcia <143185748+leogmc@users.noreply.github.com>
2023-11-16 17:50:20 -0300

    Enviando atividade atualizada

--- 16-11-23/RedeSocial/src/cliente/Usuario.java
+++ 16-11-23/RedeSocial/src/cliente/Usuario.java
@@ -1,5 +1,5 @@
 package cliente;
-
+import java.util.Scanner; //Classe de insercao de dados e etc..
 import java.util.ArrayList;
 
 public class Usuario {
@@ -31,4 +31,45 @@ public class Usuario {
 	public Usuario(){
 		postagens = new ArrayList<String>();
 	}
+	
+	public void adicionaPostagem(String post){
+		postagens.add(post);
+	}
+	
+	
+	//MAIN ------------	
+	public static void main(String[] args) {
+		
+	String postagem;	
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		Usuario usuario = new Usuario();
+		
+		   System.out.println("Insira seu nome: ");
+	       usuario.setNome(scanner.nextLine());
+	       scanner.nextLine(); 
+	       
+	       System.out.println("Insira seu e-mail: ");
+	       usuario.setEmail(scanner.nextLine());
+	       scanner.nextLine(); 
+	       
+	       System.out.println("Insira sua nacionalidade: ");
+	       usuario.setNacionalidade(scanner.nextLine());
+	       scanner.nextLine();
+	       
+	       System.out.println("Insira uma postagem: ");
+	       scanner.nextLine();
+	       postagem = scanner.nextLine();
+	       usuario.adicionaPostagem(postagem);
+	       
+	       scanner.close();
+	       
+	       //CONTINUAR NO SLIDE 14
+	       //
+	       //
+	       
+	}
 }
+
+
